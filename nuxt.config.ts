598473ca42f085c92dcb5b4ade8e788ae28edeb7
nekotyan2d <dmitryrychazhkov2019@gmail.com>
2026-04-13 01:44:05 +0300

    fix: add dotenv

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import "dotenv/config.js";
 export default defineNuxtConfig({
     compatibilityDate: "2025-07-15",
     devtools: { enabled: true },
